74a98c94c3368402a21d21ef3c32e8e512e01618
jemouraa <jecruz12@gmail.com>
2021-07-28 11:02:28 -0300

    primeiro programa C# e .Net

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Digitalinovation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int numeroDeVezes = 5;
+
+            for(int i=0; i <= numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem-vindo ao curso .Net {i}" );
+            }
+        }
+    }
+}
